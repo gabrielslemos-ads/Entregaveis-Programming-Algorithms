1710b9820d0faf231cef24b4948ab0624884231a
Gabriel Lemos <gabriel.slemos@aluno.impacta.edu.br>
2026-03-12 19:17:10 -0300

    Feat: Adição exercicio 1

---
+++ Exercicio 01.py
@@ -0,0 +1,12 @@
+n1= float (input("digite um número: "))
+
+
+n2= float (input("digite outro número: "))
+
+
+if n1 > n2:
+   print("O 1º número é maior!!!")
+elif n2 > n1:
+   print("O 2º número é maior!!!")
+else:
+   print("Os dois números são iguais!!!")
