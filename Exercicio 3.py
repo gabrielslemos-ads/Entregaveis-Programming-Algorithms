7050e53c82efd0c9e3f0fc12669a2680184cc640
Gabriel Lemos <gabriel.slemos@aluno.impacta.edu.br>
2026-03-12 20:13:34 -0300

    Feat: Adição exercicio 3

---
+++ Exercicio 3.py
@@ -0,0 +1,9 @@
+
+letra = input("Digite 'F' para Feminino ou 'M' para Masculino: ").strip().upper()
+if letra == "F":
+    print("F - Feminino")
+elif letra == "M":
+    print("M - Masculino")
+else:
+    print("Sexo Inválido.")
+
