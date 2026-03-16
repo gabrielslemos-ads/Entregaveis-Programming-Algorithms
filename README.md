65be19388ac3f112336cf81c44204c9870b606b7
Gabriel Lemos <gabriel.slemos@aluno.impacta.edu.br>
2026-03-15 21:10:52 -0300

    Apagando os arquivos Teste e README

--- README.md
+++
@@ -1,2 +0,0 @@
-# Entregaveis-Programming-Algorithms
-Listas de exercícios 01 *** Entregáveis
--- Teste.py
+++
@@ -1,2 +0,0 @@
-print ("Hello Word")
-lista de produtos = ["arroz", "feijão", "macarrão", "carne", "frutas"]
