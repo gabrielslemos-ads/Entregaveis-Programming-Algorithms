a1a6a3ae0eae6755d1e05860f1c08ae477826e88
Gabriel Lemos <gabriel.slemos@aluno.impacta.edu.br>
2026-03-15 21:39:49 -0300

    Adicionando exercício 7

---
+++ Exercicio 7.py
@@ -0,0 +1,24 @@
+# Pede os três números para o usuário
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
+
+# --- VERIFICANDO O MAIOR ---
+if num1 >= num2 and num1 >= num3:
+    maior = num1
+elif num2 >= num1 and num2 >= num3:
+    maior = num2
+else:
+    maior = num3
+
+# --- VERIFICANDO O MENOR ---
+if num1 <= num2 and num1 <= num3:
+    menor = num1
+elif num2 <= num1 and num2 <= num3:
+    menor = num2
+else:
+    menor = num3
+
+# Mostra os resultados no final
+print("O maior número é:", maior)
+print("O menor número é:", menor)
